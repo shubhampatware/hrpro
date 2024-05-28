9f0f449b02c9656fe5d14acb380511ba1b8eeb3b
Shubham Patware <shubhampatware@gmail.com>
2024-05-28 15:29:29 +0530

    sigle coute changes env variable

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@ import { createClient } from 'contentful';
 
 export async function getStaticProps() {
     const client = createClient({
-        space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-        accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+        space: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN ,
     });
 
     const webTitleLogo_Res = await client.getEntries({
