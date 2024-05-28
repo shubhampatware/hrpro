42b24049570fbe1aa9bc7298b35d54c3e55734ca
Shubham Patware <shubhampatware@gmail.com>
2024-05-28 15:14:08 +0530

    sigle coute changes config.js

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['images.ctfassets.net'],
       },
+      eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
